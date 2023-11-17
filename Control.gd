bd64aa51fa8983feb52593d6282b88e3be2456c8
Christopher Long <93620761+cvalong@users.noreply.github.com>
2023-11-17 10:55:53 -0800

    Add Label and animations for the game to tell use who wins (#2)
    * feat: computerWin label & animation, add node_animation function
    
    * feat: add playerWin & playerTie, rename computerWin to playerLose
    
    * feat: playerWin, playerLose, playerTie awaits ai selection animation

--- Control.gd
+++ Control.gd
@@ -3,44 +3,62 @@ extends Control
 
 var player
 var computer
+var computer_animation
 
 
-	
+func node_animation(node):
+	get_node(node).play('Show')
+	await get_node(node).animation_finished
+	get_node(node).stop()
+
 func computerChoice():
 	computer = randi_range(1, 3)
 	if computer == 1:
-		get_node("opponentRock/AnimationPlayer").play('Show')
-		await get_node("opponentRock/AnimationPlayer").animation_finished
-		get_node("opponentRock/AnimationPlayer").stop()
+		computer_animation = "opponentRock/AnimationPlayer"
+		node_animation(computer_animation)
 	elif computer == 2:
-		get_node("opponentPaper/AnimationPlayer").play('Show')
-		await get_node("opponentPaper/AnimationPlayer").animation_finished
-		get_node("opponentPaper/AnimationPlayer").stop()
+		computer_animation = "opponentPaper/AnimationPlayer"
+		node_animation(computer_animation)
 	elif computer == 3:
-		get_node("opponentScissors/AnimationPlayer").play('Show')
-		await get_node("opponentScissors/AnimationPlayer").animation_finished
-		get_node("opponentScissors/AnimationPlayer").stop()
+		computer_animation = "opponentScissors/AnimationPlayer"
+		node_animation(computer_animation)
 
 func playerWin():
 	if player == 1 and computer == 3:
-		print("You Win!")
+		print("Win with Rock")
+		
+		await get_node(computer_animation).animation_finished
+		node_animation("playerWin/AnimationPlayer")
 	elif player == 2 and computer == 1:
-		print("You Win!")
+		print("Win with Paper")
+		
+		await get_node(computer_animation).animation_finished
+		node_animation("playerWin/AnimationPlayer")
 	elif player == 3 and computer == 2:
-		print("You Win!")
+		print("Win with Scissors")
+		
+		await get_node(computer_animation).animation_finished
+		node_animation("playerWin/AnimationPlayer")
 	
 func playerLose():
 	if player == 1 and computer == 2:
-		print("You Lose!")
+		print("Lose")
+		await get_node(computer_animation).animation_finished
+		node_animation("playerLose/AnimationPlayer")
 	elif player == 2 and computer == 3:
-		print("You Lose!")
+		print("Lose")
+		await get_node(computer_animation).animation_finished
+		node_animation("playerLose/AnimationPlayer")
 	elif player == 3 and computer == 1:
-		print("You Lose!")
-	
+		print("Lose")
+		await get_node(computer_animation).animation_finished
+		node_animation("playerLose/AnimationPlayer")
 	
 func tie():
 	if player == computer:
 		print("Tie")
+		await get_node(computer_animation).animation_finished
+		node_animation("playerTie/AnimationPlayer")
 		
 func checkResult():
 	playerWin()
