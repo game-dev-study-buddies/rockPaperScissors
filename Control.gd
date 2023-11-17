c7e9c73d8b4c7b3a1e7ad025be93c028b6467334
brendalongbottom <151066260+brendalongbottom@users.noreply.github.com>
2023-11-17 11:51:47 -0800

    Feat: track player_win, player_lose and player_tie

--- Control.gd
+++ Control.gd
@@ -4,7 +4,9 @@ extends Control
 var player
 var computer
 var computer_animation
-
+var player_win = 0
+var player_lose = 0
+var player_tie = 0
 
 func node_animation(node):
 	get_node(node).play('Show')
@@ -26,16 +28,19 @@ func computerChoice():
 func playerWin():
 	if player == 1 and computer == 3:
 		print("Win with Rock")
+		player_win += 1
 		
 		await get_node(computer_animation).animation_finished
 		node_animation("playerWin/AnimationPlayer")
 	elif player == 2 and computer == 1:
 		print("Win with Paper")
+		player_win += 1
 		
 		await get_node(computer_animation).animation_finished
 		node_animation("playerWin/AnimationPlayer")
 	elif player == 3 and computer == 2:
 		print("Win with Scissors")
+		player_win += 1
 		
 		await get_node(computer_animation).animation_finished
 		node_animation("playerWin/AnimationPlayer")
@@ -43,20 +48,28 @@ func playerWin():
 func playerLose():
 	if player == 1 and computer == 2:
 		print("Lose")
+		player_lose += 1
+		
 		await get_node(computer_animation).animation_finished
 		node_animation("playerLose/AnimationPlayer")
 	elif player == 2 and computer == 3:
 		print("Lose")
+		player_lose += 1
+		
 		await get_node(computer_animation).animation_finished
 		node_animation("playerLose/AnimationPlayer")
 	elif player == 3 and computer == 1:
 		print("Lose")
+		player_lose += 1
+		
 		await get_node(computer_animation).animation_finished
 		node_animation("playerLose/AnimationPlayer")
 	
 func tie():
 	if player == computer:
 		print("Tie")
+		player_tie += 1
+		
 		await get_node(computer_animation).animation_finished
 		node_animation("playerTie/AnimationPlayer")
 		
@@ -64,7 +77,10 @@ func checkResult():
 	playerWin()
 	playerLose()
 	tie()
-
+	print(player_win)
+	print(player_lose)
+	print(player_tie)
+	
 func _on_rock_pressed():
 	player = 1
 	computerChoice()
@@ -79,4 +95,6 @@ func _on_scissors_pressed():
 	player = 3
 	computerChoice()
 	checkResult()
+
+
 
