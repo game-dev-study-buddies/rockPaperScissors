8cfff98adc522302e48048460f70ac1d8e8bf2a7
brendalongbottom <151066260+brendalongbottom@users.noreply.github.com>
2023-11-20 10:07:23 -0800

    Add Score tracking (#3)
    * Feat: track player_win, player_lose and player_tie
    
    * Feat: Add Global File, Win and Lose Labels with score tracking updating
    
    * Fix: Remove test prints and variable

--- Control.gd
+++ Control.gd
@@ -25,38 +25,43 @@ func computerChoice():
 
 func playerWin():
 	if player == 1 and computer == 3:
-		print("Win with Rock")
+		PlayerVariables.player_win += 1
 		
 		await get_node(computer_animation).animation_finished
 		node_animation("playerWin/AnimationPlayer")
 	elif player == 2 and computer == 1:
-		print("Win with Paper")
+		PlayerVariables.player_win += 1
 		
 		await get_node(computer_animation).animation_finished
 		node_animation("playerWin/AnimationPlayer")
 	elif player == 3 and computer == 2:
-		print("Win with Scissors")
+		PlayerVariables.player_win += 1
 		
 		await get_node(computer_animation).animation_finished
 		node_animation("playerWin/AnimationPlayer")
 	
 func playerLose():
 	if player == 1 and computer == 2:
-		print("Lose")
+		PlayerVariables.player_lose += 1
+		
 		await get_node(computer_animation).animation_finished
 		node_animation("playerLose/AnimationPlayer")
 	elif player == 2 and computer == 3:
-		print("Lose")
+		PlayerVariables.player_lose += 1
+		
 		await get_node(computer_animation).animation_finished
 		node_animation("playerLose/AnimationPlayer")
 	elif player == 3 and computer == 1:
-		print("Lose")
+		PlayerVariables.player_lose += 1
+		
 		await get_node(computer_animation).animation_finished
 		node_animation("playerLose/AnimationPlayer")
 	
 func tie():
 	if player == computer:
-		print("Tie")
+		
+		PlayerVariables.player_tie += 1
+		
 		await get_node(computer_animation).animation_finished
 		node_animation("playerTie/AnimationPlayer")
 		
@@ -65,6 +70,7 @@ func checkResult():
 	playerLose()
 	tie()
 
+	
 func _on_rock_pressed():
 	player = 1
 	computerChoice()
@@ -79,4 +85,6 @@ func _on_scissors_pressed():
 	player = 3
 	computerChoice()
 	checkResult()
+
+
 
---
+++ Global/playerVariables.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var player_win = 0
+var player_lose = 0
+var player_tie = 0
